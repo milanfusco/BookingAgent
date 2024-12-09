ec5ccf2d0f7b19d38df70f8f5094522b98e41ee7
milanfusco <milanfusco68@gmail.com>
2024-12-09 14:24:36 -0500

    Update database paths in main and test files; remove obsolete test data

--- tests/BookingAgentTests.cpp
+++ tests/BookingAgentTests.cpp
@@ -6,11 +6,11 @@ int main() {
     BookingAgent agent;
 
     // Create test cities and travel propositions
-    agent.readCities("/Users/milanfusco/Documents/repositories/BookingAgent/tests/cities.db");
+    agent.readCities("../data/cities.db");
     std::cout << "Cities added.\n";
-    agent.readTravelPropositions("/Users/milanfusco/Documents/repositories/BookingAgent/tests/travel_proposition.db");
+    agent.readTravelPropositions("../data/travel_proposition.db");
     std::cout << "Travel propositions added.\n";
-    
+
     // Test trip finding
     assert(agent.getTrip("cleveland", "algiers"));
     agent.printTrip();
